e2796250c39503cf3013664c095b571a0a1930cd
Aleh.R <aleh.rusachenka@gmail.com>
2022-11-26 14:02:17 +0300

    localStorage cart changed

--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -10,14 +10,14 @@ const cartSlice = createSlice({
   reducers: {
     setItemInCart: (state, action) => {
       state.itemsInCart.push(action.payload);
-      // localStorage.setItem("cartItems", JSON.stringify(state.itemsInCart));
+      localStorage.setItem("cartItems", JSON.stringify(state.itemsInCart));
     },
 
     deleteItemFromCart: (state, action) => {
       state.itemsInCart = state.itemsInCart.filter(
         (book) => book.id !== action.payload
       );
-      // localStorage.setItem("cartItems", JSON.stringify(state.itemsInCart));
+      localStorage.setItem("cartItems", JSON.stringify(state.itemsInCart));
     },
   },
 });
