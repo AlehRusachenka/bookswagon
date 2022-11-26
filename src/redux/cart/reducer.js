ee721b4e182d2674f483e088afdded3bc935a747
Aleh.R <aleh.rusachenka@gmail.com>
2022-11-26 13:58:17 +0300

    localStorage cart changed

--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -1,23 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const items = JSON.parse(localStorage.getItem("cartItems"));
+// const items = JSON.parse(localStorage.getItem("cartItems"));
 
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    itemsInCart: items,
+    itemsInCart: [],
   },
   reducers: {
     setItemInCart: (state, action) => {
       state.itemsInCart.push(action.payload);
-      localStorage.setItem("cartItems", JSON.stringify(state.itemsInCart));
+      // localStorage.setItem("cartItems", JSON.stringify(state.itemsInCart));
     },
 
     deleteItemFromCart: (state, action) => {
       state.itemsInCart = state.itemsInCart.filter(
         (book) => book.id !== action.payload
       );
-      localStorage.setItem("cartItems", JSON.stringify(state.itemsInCart));
+      // localStorage.setItem("cartItems", JSON.stringify(state.itemsInCart));
     },
   },
 });
